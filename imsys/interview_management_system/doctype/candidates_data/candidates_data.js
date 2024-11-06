3195d532433ef15408c15fba346d3ccda43235ca
Shivani Sahu <shivanisahu23@navgurukul.org>
2024-11-06 18:17:23 +0530

    first commit

---
+++ imsys/interview_management_system/doctype/candidates_data/candidates_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Shivani Sahu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Candidates data", {
+// 	refresh(frm) {
+
+// 	},
+// });
